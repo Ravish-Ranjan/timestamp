5c009ea965e41ebbfa5e1d0b0bb4e4871912b5c1
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-09-01 12:32:08 +0530

    project done

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 
 app.use(cors({ optionsSuccessStatus: 200 }));
 
-app.get("/api/:date?", (req, res) => {
+app.get("/api/:date?", (req, res, done) => {
     const { date } = req.params;
 
     if (!date) {
         let cur = new Date();
-        res.status(200).send({
-            unix: Math.floor(cur.getTime() / 1000),
+        return res.status(200).json({
+            unix: Math.floor(cur.getTime()),
             utc: cur.toUTCString(),
         });
     }
@@ -25,20 +25,20 @@ app.get("/api/:date?", (req, res) => {
         const timestamp = Number(date);
         const date_ret = new Date(timestamp);
         if (!isNaN(date_ret.getTime())) {
-            result["unix"] = Math.floor(date_ret.getTime() / 1000);
-            result["utc"] = date_ret.toUTCString();
-            res.status(200).json(result);
+            result.unix = Math.floor(date_ret.getTime());
+            result.utc = date_ret.toUTCString();
+            return res.status(200).json(result);
         } else {
-            res.status(400).json({ error: "Invalid Date" });
+            return res.status(400).json({ error: "Invalid Date" });
         }
     } else {
         const dateObj = new Date(date);
         if (isNaN(dateObj.getTime())) {
             return res.status(400).json({ error: "Invalid Date" });
         }
-        result["unix"] = Math.floor(dateObj.getTime() / 1000);
-        result["utc"] = dateObj.toUTCString();
-        res.status(200).json(result);
+        result.unix = Math.floor(dateObj.getTime());
+        result.utc = dateObj.toUTCString();
+        return res.status(200).json(result);
     }
 });
 
